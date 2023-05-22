e88de8008ae77c363061cbd9d95cc2f4b23fceba
John Rasine Irem <rajonirem8@gmail.com>
2023-05-22 07:13:51 +0100

    bug fix: no more errors on text editor ref

---
+++ src/components/TextEditor.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, useRef,  } from "react"
+export const TextEditor=()=>{
+  const textAreaRef= useRef<HTMLTextAreaElement>(null)
+  const [textValue, setTextValue]= useState('')
+  const cursor= textAreaRef.current?.selectionStart
+  useEffect(()=>{
+    setTextValue('')
+    // textAreaRef.current=textValue
+  },[])
+  console.log(textAreaRef)
+
+  const handleLink=():void=>{
+    const words= textValue?.split(/\s+/)
+    const newTextValue=words?.slice(0, cursor).concat(`[]() ${words.slice(cursor, words.length)}` ).join(' ')
+    setTextValue(newTextValue)
+  }
+  return(
+    <>
+    <div>
+      <textarea 
+      placeholder="your text here"
+      id="text"
+      // value={textValue}
+      value={textValue}
+      onChange={(e) => setTextValue(e.target.value)}
+      ref={textAreaRef}
+      >
+
+      </textarea>
+    </div>
+    <div className="btn-container">
+      <button onClick={handleLink}>link</button>
+      <button>italic</button>
+      <button>bold</button>
+    </div>
+  
+  </>
+  )
+}
