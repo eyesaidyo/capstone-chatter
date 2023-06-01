312ef034a78d2d719ab331f343dc50c3a61923df
John Rasine Irem <rajonirem8@gmail.com>
2023-06-01 17:02:54 +0100

    feat: landing 1st sec done

--- src/routes/landing-page/landing-page-styles.tsx
+++ src/routes/landing-page/landing-page-styles.tsx
@@ -11,7 +11,17 @@ height: 100vh;
     position:absolute ;
     width: 100%;
     height: inherit;
-    text-align: center;
+    /* text-align: center; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+  }
+  & .align{
+    width: 70%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 150px;
   }
   & img{
     width: 100%;
@@ -20,8 +30,30 @@ height: 100vh;
     /* display: none; */
     
   }
-  /* & h1{
-    margin-top:500px ;
+  & h1{
+    font-weight: 700;
+    font-size: 3rem;
+    margin-top: -200px;
 
-  } */
+  }
+  & p{
+    font-size: 1.5rem;
+    font-weight:500;
+    margin-top: 50px;
+  }
+  & button{
+    background: #543EE0;
+    border-radius: 8px;
+    color: #fff;
+    border: 1px solid #543EE0;
+    width: 200px;
+    text-align: center;
+    padding: 20px;
+    font-size: 1.2rem;
+    margin-top:20px;
+     :hover{
+      background: rgb(0, 40, 90);
+      cursor: pointer;
+    }
+  }
 `
--- src/routes/landing-page/landing-page.tsx
+++ src/routes/landing-page/landing-page.tsx
@@ -5,8 +5,11 @@ export const LandingPage=()=>{
   <LandingPageWrap>
     
     <div className="text">
-    <h1>Welcome to Chatter: A Haven for Text-Based Content</h1>
-    <p>Unleash the Power of Words. Connect with Like-Minded Readers and Writers</p>
+      <div className="align">
+      <h1>Welcome to Chatter: A Haven for Text-Based Content</h1>
+      <p>Unleash the Power of Words. Connect with Like-Minded Readers and Writers</p>
+      <button>Get Started</button>
+      </div>
     </div>
   </LandingPageWrap>
   )
