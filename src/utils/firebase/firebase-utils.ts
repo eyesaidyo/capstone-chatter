ae565d75bcb351b080e1351b6bc09d64b80e1544
John Rasine Irem <rajonirem8@gmail.com>
2023-06-06 23:25:12 +0100

    user auth typed

--- src/utils/firebase/firebase-utils.ts
+++ src/utils/firebase/firebase-utils.ts
@@ -1,12 +1,14 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { FirebaseError, initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
   signInWithPopup,
   GoogleAuthProvider,
-  getAuth
+  getAuth,
+  
+  User
 } from 'firebase/auth'
 import {
   getFirestore,
@@ -14,7 +16,7 @@ import {
   getDoc,
   setDoc,
   collection,
-  writeBatch,
+  
   query,
   getDocs,
 } from "firebase/firestore";
@@ -29,11 +31,41 @@ const firebaseConfig = {
   appId: "1:718377893836:web:91b6072fba20c6f05da769",
   measurementId: "G-Z67HQJMXVT"
 };
-
+import 'firebase/auth'
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const db =getFirestore()
 const googleProvider = new GoogleAuthProvider()
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
+  export const createUserDocFromAuth = async (userAuth:User) => {
+    if (!userAuth) return;
+    const userDocRef = doc(db, "users", userAuth.uid);
+    // console.log(userDocRef);
+    const userSnapshot = await getDoc(userDocRef);
+    //console.log(userSnapshot);
+    if (!userSnapshot.exists()) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+      const notes:string[] = [];
+      try {
+        await setDoc(userDocRef, {
+          displayName,
+          email,
+          createdAt,
+          notes,
+        });
+      } catch (error) {
+        if (error instanceof FirebaseError) {
+          // Firebase error
+          console.error('Firebase error:', error.message);
+        } else {
+          // Other error types
+          console.error('Error:', error);
+        }
+      }
+    }
+    return userDocRef;
+  };
