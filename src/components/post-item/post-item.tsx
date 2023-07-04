d3acd390d39aeea26a3afb9d6d29057c6c6bbdb4
John Rasine Irem <rajonirem8@gmail.com>
2023-07-04 21:44:29 +0100

    not much

--- src/components/post-item/post-item.tsx
+++ src/components/post-item/post-item.tsx
@@ -1,13 +1,13 @@
-import { PostWrap } from "./post-item-styles"
-export interface PostProps{
-  avatarSRC?:string;
-  name?:string;
-  date:number;
-  title:string;
-  summary:string;
-  articleSRC?:string;
+import { PostWrap } from "./post-item-styles";
+export interface PostProps {
+  avatarSRC?: string;
+  name?: string;
+  date: number;
+  title: string;
+  content: string;
+  articleSRC?: string;
 }
-export const PostItem=(props:PostProps)=>{
+export const PostItem = (props: PostProps) => {
   return (
     <PostWrap>
       <div className="top">
@@ -16,11 +16,9 @@ export const PostItem=(props:PostProps)=>{
         <p>{props.date}</p>
       </div>
       <h3>{props.title}</h3>
-      <p>{props.summary}</p>
-      <img className="article-img" src={props.articleSRC}/>
-      <div className="foot">
-
-      </div>
+      <p>{props.content}</p>
+      <img className="article-img" src={props.articleSRC} />
+      <div className="foot"></div>
     </PostWrap>
-  )
-}
+  );
+};
--- src/routes/feed/feed.tsx
+++ src/routes/feed/feed.tsx
@@ -1,35 +1,60 @@
-import { PostItem,  } from "../../components/post-item/post-item"
-import { FeedWrap } from "./feed-styles"
-import pfp from '../../assets/postPFP.svg'
-import img from '../../assets/postIMG.svg'
-import { useEffect, useState } from "react"
-import { getGlobalPosts } from "../../utils/firebase/firebase-utils"
+import { PostItem } from "../../components/post-item/post-item";
+import { FeedWrap } from "./feed-styles";
+import pfp from "../../assets/postPFP.svg";
+import img from "../../assets/postIMG.svg";
+import { useEffect, useState } from "react";
+import { getGlobalPosts } from "../../utils/firebase/firebase-utils";
 
-export const Feed=()=>{
-   const [feed, setFeed] = useState([{content:'asd'}])
+export const Feed = () => {
+  const [feed, setFeed] = useState([
+    {
+      avatarSRC: "",
+      name: "",
+      date: "",
+      title: "",
+      content: "",
+      articleSRC: "",
+    },
+  ]);
   // const getFeed=()=>{
   //   console.log('fetchin feed...')
   //   getGlobalPosts()
   // }
-  useEffect(
-    ()=>{
-      getGlobalPosts().then(res=>setFeed(res))
-      console.log(`feed ${feed[0].content}`)
-    },[]
-  )
-  const date = new Date()
+  useEffect(() => {
+    getGlobalPosts().then((res) => setFeed(res));
+    console.log(`feed ${feed[0].content}`);
+  }, []);
+  const date = new Date();
   return (
     <>
-    
-    <FeedWrap>
-      <h2>feed</h2>
-      <PostItem title="how to train your dragon" avatarSRC={pfp} articleSRC={img} summary='a story on dragon training' date={date.getDate()} name='rajon irem' />
-      <PostItem title="how to train your dragon" avatarSRC={pfp} articleSRC={img} summary='a story on dragon training' date={date.getDate()} name='rajon irem' />
-      <PostItem title="how to train your dragon" avatarSRC={pfp} articleSRC={img} summary='a story on dragon training' date={date.getDate()} name='rajon irem' />
-      <h1>{feed[0].content }</h1>
-    </FeedWrap>
-    
-    
+      <FeedWrap>
+        <h2>feed</h2>
+        <PostItem
+          title="how to train your dragon"
+          avatarSRC={pfp}
+          articleSRC={img}
+          content="a story on dragon training"
+          date={date.getDate()}
+          name="rajon irem"
+        />
+        <PostItem
+          title="how to train your dragon"
+          avatarSRC={pfp}
+          articleSRC={img}
+          content="a story on dragon training"
+          date={date.getDate()}
+          name="rajon irem"
+        />
+        <PostItem
+          title="how to train your dragon"
+          avatarSRC={pfp}
+          articleSRC={img}
+          content="a story on dragon training"
+          date={date.getDate()}
+          name="rajon irem"
+        />
+        <h1>{feed[0].content}</h1>
+      </FeedWrap>
     </>
-  )
-}
+  );
+};
