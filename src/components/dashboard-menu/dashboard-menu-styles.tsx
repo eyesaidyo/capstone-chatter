e2dd5b09894a3c2ddf815f7973cb3dc154156abc
John Rasine Irem <rajonirem8@gmail.com>
2023-08-05 00:03:11 +0100

    dash

--- src/components/dashboard-menu/dashboard-menu-styles.tsx
+++ src/components/dashboard-menu/dashboard-menu-styles.tsx
@@ -1,43 +1,44 @@
 import styled from "styled-components";
 
-export const MenuWrap=styled.div`
-  color:#543ee0;
+export const MenuWrap = styled.div`
+  color: #543ee0;
   height: 100vh;
   /* position: fixed; */
   overflow-y: hidden;
   width: 317px;
   display: flex;
   flex-direction: column;
-  gap:20px;
+  gap: 20px;
   padding: 20px;
   border-right: 1px solid #1e1e1e;
-  
-  & h1{
+
+  & h1 {
     margin-bottom: 30px;
   }
-  & button{
+  & button {
     padding: 10px;
     color: #fff;
     background-color: #543ee0;
     border: 1px #543ee0;
     border-radius: 8px;
-    
-  }
-  .logout{
-    color:red;
-    & :hover{
+    :hover {
       cursor: pointer;
     }
   }
-`
-export const ItemWrap= styled.div`
-font-size: 1.2rem;
-display: flex;
-justify-content: baseline;
-  & img{
+  & .logout {
+    color: red;
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
+export const ItemWrap = styled.div`
+  font-size: 1.2rem;
+  display: flex;
+  justify-content: baseline;
+  & img {
     width: 1.5rem;
     margin-right: 20px;
     /* align-self: baseline; */
   }
- 
-`
+`;
